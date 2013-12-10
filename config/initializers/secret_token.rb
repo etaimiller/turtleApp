390e829e7dcd286f43b1b339b2b8da052f8182c7
etaimiller <etaimills@gmail.com>
2013-12-10 14:45:02 +0200

    initial commit with devise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TurtleApp::Application.config.secret_key_base = '3eddba938137592e71f4aa40fccad8b2c317de0ee6592be16e5bceff7a154bdf748b7ba12d801173db8e072824c4561a6007485c49f80532c771683e9ead8e18'
